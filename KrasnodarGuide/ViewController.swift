0b156e67556bd317a92a6b9e4c57639fd5df8dbe
ulkoart <ulkoart@gmail.com>
2021-03-25 15:56:03 +0300

    Initial Commit

---
+++ KrasnodarGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KrasnodarGuide
+//
+//  Created by user on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
