fe39fb1e62831f13348b59bed779917b89dd04f2
ulkoart <ulkoart@gmail.com>
2021-05-14 13:25:13 +0300

    update person detail screen

--- KrasnodarGuide/Controller/Person/PersonDetail.swift
+++ KrasnodarGuide/Controller/Person/PersonDetail.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class PersonDetail: UIViewController {
     
+    @IBOutlet weak var personPhotoCollection: UICollectionView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var personPhotoCollectionView: UICollectionView!
     
@@ -26,19 +27,31 @@ class PersonDetail: UIViewController {
         personPhotoCollectionView.delegate = self
         personPhotoCollectionView.dataSource = self
         personPhotoCollectionView.showsHorizontalScrollIndicator = false
-        personPhotoCollectionView.register(UINib(nibName: cellId, bundle: nil), forCellWithReuseIdentifier: cellId)
+        personPhotoCollectionView.register(PersonDetailCell.self, forCellWithReuseIdentifier: cellId)
     }
 }
 
 extension PersonDetail: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? PersonDetailCell else { return UICollectionViewCell() }
+        cell.setup(with: person.image)
         return cell
     }
     
     
+}
+
+
+extension PersonDetail: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return personPhotoCollection.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
--- KrasnodarGuide/Controller/Person/PersonDetailCell.swift
+++ KrasnodarGuide/Controller/Person/PersonDetailCell.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class PersonDetailCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private var personPhoto: UIImageView  = UIImageView(image: UIImage())
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(personPhoto)
+        personPhoto.translatesAutoresizingMaskIntoConstraints = false
+        personPhoto.contentMode = .scaleAspectFill
+        
+        personPhoto.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        personPhoto.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        personPhoto.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        personPhoto.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(with photoName: String) {
+        self.personPhoto.image = UIImage(named: photoName)
+    }
 }
